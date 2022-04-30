a5f11198e205eb399a66d2de45b4fe7f60e17643
Wth10 <weuberth10@gmail.com>
2022-04-30 17:02:02 -0300

    UpdateForce

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,9 +2,7 @@ export default function Home() {
 	return (
 		<div>
 			<h1>Welcome</h1>
-			<p>
-				Pagina <a href="/teste">2</a>
-			</p>
+			<p>Pagina</p>
 		</div>
 	);
 }
--- src/pages/teste.jsx
+++ src/pages/teste.jsx
@@ -2,9 +2,7 @@ export default function Home() {
 	return (
 		<div>
 			<h1>Welcome</h1>
-			<p>
-				Pagina <a href="/">teste</a>
-			</p>
+			<p>Pagina</p>
 		</div>
 	);
 }
