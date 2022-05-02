767172c6c8f40710c3adaa7e5663c80c12287d1c
Wth10 <weuberth10@gmail.com>
2022-05-02 01:11:49 -0300

    UpdateHead

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,6 +3,11 @@ import Link from 'next/link';
 export default function About() {
 	return (
 		<div>
+			<header>
+				<title>Next-About</title>
+				<script src="https://cdn.tailwindcss.com"></script>
+			</header>
+
 			<h1>Welcome About</h1>
 			<Link href="/">
 				<a>Voltar Página</a>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,23 @@
 import Link from 'next/link';
+import Head from '../layout/Head';
 
 export default function Home() {
 	return (
-		<div>
-			<h1>Welcome Home</h1>
-			<Link href="/about">
-				<a>About Us</a>
-			</Link>
-		</div>
+		<main>
+			<header>
+				<title>Next-App</title>
+				<script src="https://cdn.tailwindcss.com"></script>
+				<meta charset="UTF-8" />
+				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			</header>
+			<div>
+				<Head />
+				<h1>Welcome Home</h1>
+				<Link href="/about">
+					<a>About Us</a>
+				</Link>
+			</div>
+		</main>
 	);
 }
