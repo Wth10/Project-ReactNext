5d5bc23f01efc081c4270988f515564aae546bf9
Wth10 <weuberth10@gmail.com>
2022-05-26 22:41:28 -0300

    UpdateFiles

--- src/layout/Login.jsx
+++ src/layout/Login.jsx
@@ -54,11 +54,14 @@ export default function Login() {
 						</div>
 					</div>
 					<div class="mt-8">
+						<input
+							type="submit"
+							class="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 text-sm font-semibold leading-none text-white focus:outline-none bg-indigo-700 border rounded hover:bg-indigo-600 py-4 w-full"
+						/>
 						<a href="/home">
-							<input
-								type="submit"
-								class="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 text-sm font-semibold leading-none text-white focus:outline-none bg-indigo-700 border rounded hover:bg-indigo-600 py-4 w-full"
-							/>
+							<button className="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 text-sm font-semibold leading-none text-white focus:outline-none bg-indigo-700 border rounded hover:bg-indigo-600 py-4 w-full">
+								Pagína Home
+							</button>
 						</a>
 					</div>
 				</form>
