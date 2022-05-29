d3ad375fac01b99f86e6c37b23cf8a802a0533a0
Wth10 <weuberth10@gmail.com>
2022-05-28 21:10:18 -0300

    UpdateChildren

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,12 +1,11 @@
-export default function button(props) {
+export default function button({children}) {
 	return (
 		<>
 			<button
-				onClick={props.onClick}
 				type="button"
 				className="button w-full sm:w-auto py-2 px-3 bg-indigo-600 hover:bg-indigo-700 disabled:bg-indigo-300 dark:disabled:bg-indigo-800 text-white dark:disabled:text-indigo-400 text-sm font-semibold rounded-md shadow focus:outline-none cursor-pointer"
 			>
-				{props.name}
+				{children}
 			</button>
 		</>
 	);
